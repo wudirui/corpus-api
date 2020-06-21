d96887601988d9d93bff892a6c44f8811e8a4471
superrui <1570266831@qq.com>
2020-06-21 21:11:14 +0800

    接收微信小程序的语音及其参数的上传

--- src/main/java/com/zr/corpus/controller/SendReceiveController.java
+++ src/main/java/com/zr/corpus/controller/SendReceiveController.java
@@ -1,8 +1,10 @@
 package com.zr.corpus.controller;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import com.sun.deploy.util.StringUtils;
 import com.zr.corpus.common.PageUtils;
 import com.zr.corpus.service.CorpusService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +13,8 @@ import org.springframework.web.multipart.MultipartFile;
 import sun.misc.BASE64Encoder;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @RestController
 @RequestMapping(value = "/wch")
@@ -23,16 +23,16 @@ public class SendReceiveController {
     CorpusService corpusService;
 
     @RequestMapping(value = "getSentence", method = RequestMethod.POST)
-    public Object receiveMsg(@RequestBody JSONObject params) {
-        int index = params.getInteger("index");
+    public Object receiveMsg(@RequestBody JSONObject jsonObject) {
+        JSONObject params = jsonObject.getJSONObject("params");
+        Integer index = jsonObject.getInteger("index");
+        String region = StringUtils.join(params.getJSONArray("region"),",");
+        params.put("region",region);
+//        String name = params.getString("name");
+//        String dialect = params.getString("dialect");
+//        String sex = params.getString("sex");
         PageHelper.startPage(index, 1);
-        Iterator it = params.entrySet().iterator();
-        while (it.hasNext()) {
-            Map.Entry entry = (Map.Entry) it.next();
-            if (entry.getValue() == null || entry.getValue() == "") {
-                params.put((String) entry.getKey(), null);
-            }
-        }
+
         List<Map<String, Object>> list = corpusService.getOne(params);
         PageInfo<Map<String, Object>> info = new PageInfo<>(list);
         PageUtils pageUtils = new PageUtils();
@@ -46,11 +46,13 @@ public class SendReceiveController {
     public String sendMsg(@RequestParam("audioFile") MultipartFile audioFile, @RequestParam("name") String name,
                           @RequestParam("sex") String sex, @RequestParam("address") String address,
                           @RequestParam("dialect") String dialect, @RequestParam("sentenceId") String sentenceId) throws IOException {
-
         String preffix = "data:audio/wav;base64,";
         BASE64Encoder base64Encoder = new BASE64Encoder();
         String audioStr = preffix + base64Encoder.encode(audioFile.getBytes()).replaceAll("[\\s*\t\n\r]", "");
 
+        Date date = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String createTime = formatter.format(date);
         Map<String, String> map = new HashMap<>();
         map.put("name", name);
         map.put("audioStr", audioStr);
@@ -58,6 +60,7 @@ public class SendReceiveController {
         map.put("dialect", dialect);
         map.put("address", address);
         map.put("sentenceId", sentenceId);
+        map.put("createTime", createTime);
         corpusService.addCorpus(map);
         System.out.println(name);
         return "ok";
